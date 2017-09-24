0d2a9674009648f0275d33f4781739f882d2768f
iamplm <wanghuaijie0@126.com>
2017-09-24 10:24:28 +0000

    hugo

---
+++ post/a.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import pdb
+#pdb.set_trace()
+def func(n):
+	f=open(n,'r')
+	s=f.read()
+	f.close()
+	f=open(n,'w')
+	x=s.split('\n')
+	x.insert(0,'---')
+	x.insert(4,'---')
+	x[1]='title: "'+x[1][7:]
+	x[1]+='"'
+	x[2]='tags: ['+x[2][6:]
+	x[2]+=']'
+	s1=x[3].split('-')
+	if (len(s1[1])==1): s1[1]='0'+s1[1]
+	if (len(s1[2])==1): s1[2]='0'+s1[2]
+	x[3]='-'.join(s1)
+	x[3]='d'+x[3][1:]+'T10:02:43Z'
+	f.write('\n'.join(x))
+	f.close()
+for a,b,c in os.walk('./'):
+	for n in c:
+		if (n.endswith('.md')):
+			func(n)
